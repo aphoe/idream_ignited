6de93e5242053ff8a4a505c0d622378b633c022f
afolabi legunsen <aphoe@outlook.com>
2013-08-31 20:17:33 +0100

    Mods

--- application/views/auth/forgot_password.php
+++ application/views/auth/forgot_password.php
@@ -1,4 +1,4 @@
-<h1><?php echo lang('forgot_password_heading');?></h1>
+<h1><span class="icon-user"></span><?php echo lang('forgot_password_heading');?></h1>
 <p><?php echo sprintf(lang('forgot_password_subheading'), $identity_label);?></p>
 
 <div id="infoMessage"><?php echo $message;?></div>
@@ -6,10 +6,10 @@
 <?php echo form_open("auth/forgot_password");?>
 
       <p>
-      	<label for="email"><?php echo sprintf(lang('forgot_password_email_label'), $identity_label);?></label> <br />
-      	<?php echo form_input($email);?>
+      	
+      	<?php echo form_input($email, '', 'placeholder="Email address"');?>
       </p>
 
-      <p><?php echo form_submit('submit', lang('forgot_password_submit_btn'));?></p>
+      <p><?php echo form_submit('submit', lang('forgot_password_submit_btn'), 'class="btn"');?></p>
 
 <?php echo form_close();?>
