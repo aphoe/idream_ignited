c626bd3c8bfd80946098a9514671072bd4bbc3bb
afolabi legunsen <aphoe@outlook.com>
2013-08-31 17:07:12 +0100

    Mods

---
+++ application/controllers/login.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Controller that handles the login and 
+ * registration functions of the site
+ * 
+ * @author Afolabi  Legunsen <aphoe@outlook.com>
+ * @copyright (c) 2013
+ */
+
+class Login extends CI_Controller {
+    
+    function __construct() {
+        parent::__construct();
+        $this->load->helper('form');
+        $this->load->library('ion_auth');
+        $th
+    }
+
+
+    function index()
+    {
+        if($this->io)
+    }
+}
