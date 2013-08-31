b1f049df3dd21f0bdaa8782104efb08ed0f71d24
afolabi legunsen <aphoe@outlook.com>
2013-08-31 16:36:51 +0100

    early changes to merge registration and login page. makes the registration view kinda useless

--- application/views/login.php
+++ application/views/login.php
@@ -10,7 +10,37 @@
 //Call form helper
 $this->load->helper('form');
 
-//Display form
+//Display Login form
+echo form_open("", 'class="form-horizontal" id="logfrm"');
+echo form_fieldset();
+
+//Email field
+$data_form = array(
+    'type' => 'email',
+    'name' => 'email',
+    'id' => 'email',
+    'placeholder' => 'Email address',
+    'required' => 'true'
+);
+echo form_input($data_form);
+
+//Password
+$data_form = array(
+    'name' => 'pword',
+    'id' => 'pword',
+    'placeholder' => 'Password',
+    'required' => 'true'
+);
+echo form_password($data_form);
+
+//Login button
+form_submit('login', 'LOGIN', 'class="pull-right" id="login"');
+
+
+echo form_fieldset_close();
+echo form_close();
+
+//Display registration form
 ?>
 <h2><span class="icon-user"></span>User login</h2>
 <?php
@@ -20,11 +50,71 @@ $form_attrib = array(
 );
 echo form_open('', $form_attrib);
 echo form_fieldset();
-//
-echo '<div class="control-group>';
+//Full name field
+$data_form = array(
+    'type' => 'text',
+    'placeholder' => 'Full name',
+    'id' => 'fullname',
+    'name' => 'fullname',
+    'required' => 'true',
+    'class' => ''
+);
+echo form_input($data_form);
 
-echo '</div>';
+//Email
+$data_form = array(
+    'type' => 'email',
+    'placeholder' => 'Email address',
+    'id' => 'mail',
+    'name' => 'mail',
+    'required' => 'true',
+    'class' => ''
+);
+echo form_input($data_form);
 
+//Password
+$data_form = array(
+    'placeholder' => 'Password',
+    'id' => 'pword',
+    'name' => 'pword',
+    'required' => 'true',
+    'class' => ''
+);
+echo form_password($data_form);
+
+//Confirm password
+$data_form = array(
+    'type' => 'password',
+    'placeholder' => 'Confirm password',
+    'id' => 'cpwd',
+    'name' => 'cpwd',
+    'required' => 'true',
+    'class' => ''
+);
+echo form_password($data_form);
+
+//Accept site terms and condition
+$data_form = array(
+    'id' => 'chktos',
+    'required' => 'true'
+);
+
+echo form_input($data_form);
+
+//Accept terms
+echo form_checkbox('chktos', 
+        'I agree to <a href="#">Terms</a> and <a href="#">Policy</a> of the site', 
+        FALSE, 
+        $data_form);
+//Submit form
+$data_form = array(
+    'value' => 'SIGN UP',
+    'name' => 'submit',
+    'id' => 'submit',
+    'type' => 'submit',
+    'class' => 'btn pull-right',
+);
+echo form_submit($data_form);
 echo form_fieldset_close();
 
 
